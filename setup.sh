e2326e2404e91c8c0800235a8e1033be75258200
pavanis333 <pavanis333@gmail.com>
2021-05-26 22:01:43 +0530

    c

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-yum install -y yum-ytils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-yum -y install terraform
-yum install docker -y
+yum install -y utils
+yum-config-manager --ad-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum install -y terraform
+yum install -y docker
 systemctl start docker
-mkdir /root/.aws/
-echo "Paste AWS credentials and press 2 CTRL + D"
+mkdir /root/.aws
+echo "Paste AWS credentials and press CTRL + D 2 times"
 cat > /root/.aws/credentials
 terraform init
 terraform apply
